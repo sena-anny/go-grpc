9e1957b1923c274fbe344a40c0d3905765cf9164
KJ <cozykoji18@gmail.com>
2020-08-27 00:06:28 +0900

    pointer

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/sena-anny/go-grpc/animals"
+
+	_ "github.com/sena-anny/go-grpc/animals"
 )
 
 func main() {
+	var p int
+	a := &p
+	p = 5
+	fmt.Printf("typs = %T adress = %p value = %d \n",a, a, *a)
+	*a = 10
+
+	s := "PS"
+	printString(s)
+
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.DogFeed())
+}
+
+func printString(s string) {
+	fmt.Println(s)
 }
 
 func test() string {
