0e22193656b2b3ec7ce456dd7a655168cd7cf185
KJ <cozykoji18@gmail.com>
2020-08-27 23:52:41 +0900

    interface

--- main.go
+++ main.go
@@ -17,12 +17,29 @@ func main() {
 	s := "PS"
 	printString(s)
 
+	n := Sum(
+			[]int{1,2,3},
+			func(i int) int {
+				return i *2
+			},
+		)
+	fmt.Println(n)
+
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.DogFeed())
 }
 
 func printString(s string) {
 	fmt.Println(s)
+}
+
+type Callback func(i int) int
+func Sum(ints []int, callback Callback) int {
+	var sum int
+	for _, i := range ints {
+		sum += i
+	}
+	return callback(sum)
 }
 
 func test() string {
